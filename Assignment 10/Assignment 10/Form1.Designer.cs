c151a54a2ee3adad2fb4a4f5618fb4ede34f9be8
Jaden Whitt <jadenmike31@gmail.com>
2026-02-20 13:37:24 -0500

    Add files via upload

---
+++ Assignment 10/Assignment 10/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Assignment_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoursWorked = new System.Windows.Forms.Label();
+            this.hourlyPayRate = new System.Windows.Forms.Label();
+            this.grossPay = new System.Windows.Forms.Label();
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.hourlyPayRateTextBox = new System.Windows.Forms.TextBox();
+            this.grossPayLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hoursWorked
+            // 
+            this.hoursWorked.AutoSize = true;
+            this.hoursWorked.Location = new System.Drawing.Point(21, 12);
+            this.hoursWorked.Name = "hoursWorked";
+            this.hoursWorked.Size = new System.Drawing.Size(93, 16);
+            this.hoursWorked.TabIndex = 0;
+            this.hoursWorked.Text = "Hours worked:";
+            // 
+            // hourlyPayRate
+            // 
+            this.hourlyPayRate.AutoSize = true;
+            this.hourlyPayRate.Location = new System.Drawing.Point(12, 36);
+            this.hourlyPayRate.Name = "hourlyPayRate";
+            this.hourlyPayRate.Size = new System.Drawing.Size(101, 16);
+            this.hourlyPayRate.TabIndex = 1;
+            this.hourlyPayRate.Text = "Hourly pay rate:";
+            // 
+            // grossPay
+            // 
+            this.grossPay.AutoSize = true;
+            this.grossPay.Location = new System.Drawing.Point(42, 63);
+            this.grossPay.Name = "grossPay";
+            this.grossPay.Size = new System.Drawing.Size(72, 16);
+            this.grossPay.TabIndex = 2;
+            this.grossPay.Text = "Gross pay:";
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(113, 6);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(100, 22);
+            this.hoursWorkedTextBox.TabIndex = 3;
+            this.hoursWorkedTextBox.TextChanged += new System.EventHandler(this.hoursWorkedTextBox_TextChanged);
+            // 
+            // hourlyPayRateTextBox
+            // 
+            this.hourlyPayRateTextBox.Location = new System.Drawing.Point(113, 33);
+            this.hourlyPayRateTextBox.Name = "hourlyPayRateTextBox";
+            this.hourlyPayRateTextBox.Size = new System.Drawing.Size(100, 22);
+            this.hourlyPayRateTextBox.TabIndex = 4;
+            this.hourlyPayRateTextBox.TextChanged += new System.EventHandler(this.hourlyPayRateTextBox_TextChanged);
+            // 
+            // grossPayLabel
+            // 
+            this.grossPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grossPayLabel.Location = new System.Drawing.Point(113, 62);
+            this.grossPayLabel.Name = "grossPayLabel";
+            this.grossPayLabel.Size = new System.Drawing.Size(100, 23);
+            this.grossPayLabel.TabIndex = 5;
+            this.grossPayLabel.Click += new System.EventHandler(this.grossPayLabel_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(1, 99);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(90, 46);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate Gross Pay";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(97, 99);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(88, 46);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(191, 99);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(89, 46);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 175);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.grossPayLabel);
+            this.Controls.Add(this.hourlyPayRateTextBox);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.grossPay);
+            this.Controls.Add(this.hourlyPayRate);
+            this.Controls.Add(this.hoursWorked);
+            this.Name = "Form1";
+            this.Text = "Payroll With Overtime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hoursWorked;
+        private System.Windows.Forms.Label hourlyPayRate;
+        private System.Windows.Forms.Label grossPay;
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.TextBox hourlyPayRateTextBox;
+        private System.Windows.Forms.Label grossPayLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
